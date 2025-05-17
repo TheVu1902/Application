e345edae5cdca8c32f18d1dcdf18ad765ca023d9
ChuongDuong1504 <duong13242005@gmail.com>
2025-05-17 07:54:32 +0700

    init project

---
+++ Cong/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Cong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSo1 = new System.Windows.Forms.Label();
+            this.lblSo2 = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSo1
+            // 
+            this.lblSo1.AutoSize = true;
+            this.lblSo1.Location = new System.Drawing.Point(164, 157);
+            this.lblSo1.Name = "lblSo1";
+            this.lblSo1.Size = new System.Drawing.Size(109, 16);
+            this.lblSo1.TabIndex = 0;
+            this.lblSo1.Text = "Nhập số thứ nhất:\n";
+            // 
+            // lblSo2
+            // 
+            this.lblSo2.AutoSize = true;
+            this.lblSo2.Location = new System.Drawing.Point(164, 225);
+            this.lblSo2.Name = "lblSo2";
+            this.lblSo2.Size = new System.Drawing.Size(102, 16);
+            this.lblSo2.TabIndex = 1;
+            this.lblSo2.Text = "Nhập số thứ hai:\n";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(164, 268);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(55, 32);
+            this.lblKetQua.TabIndex = 2;
+            this.lblKetQua.Text = "Kết quả:\n\n";
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(304, 151);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(100, 22);
+            this.txtSo1.TabIndex = 3;
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(304, 219);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(100, 22);
+            this.txtSo2.TabIndex = 4;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(167, 185);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 23);
+            this.btnCong.TabIndex = 5;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.lblSo2);
+            this.Controls.Add(this.lblSo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSo1;
+        private System.Windows.Forms.Label lblSo2;
+        private System.Windows.Forms.Label lblKetQua;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.Button btnCong;
+    }
+}
+
---
+++ Cong/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Cong
+
+{
+    public partial class Form1 : Form
+    {
+        // Constructor khởi tạo giao diện
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sự kiện nút "Cộng"
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double so1 = double.Parse(txtSo1.Text);
+                double so2 = double.Parse(txtSo2.Text);
+                double tong = so1 + so2;
+                lblKetQua.Text = "Kết quả: " + tong.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vui lòng nhập số hợp lệ.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
